02f31c406394d9e60170cdd04fee2a0e7f8bac4e
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-04 09:45:36 +0545

    day14 enums

--- day15_enums/src/main.rs
+++ day15_enums/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+enum IpAddrKind{
+    V4,
+    V6
+}
+
+fn main(){
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    println!("{:?}",six);
+    println!("{:?}",four);
 }
