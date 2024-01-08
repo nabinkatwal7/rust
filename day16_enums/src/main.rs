c9c16d6ad89a1a88f4cfa45d8e21cc35fb8ed3f8
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-08 16:06:21 +0545

    day16 enums completed

--- day16_enums/src/main.rs
+++ day16_enums/src/main.rs
@@ -1,19 +1,40 @@
+#[derive(Debug)]
+enum UsState{
+    Alabama,
+    Alaska,
+    Arizona,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,    
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let penny = Coin::Penny;
+    let nickel = Coin::Nickel;
+    let dime = Coin::Dime;
+    let quarter = Coin::Quarter;
+
+    println!("{}", value_in_cents(penny));
+    println!("{}", value_in_cents(nickel));
+    println!("{}", value_in_cents(dime));
+    println!("{}", value_in_cents(quarter(UsState::Alabama)));
 }
