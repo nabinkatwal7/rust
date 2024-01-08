e5ebae839db84b66e9e529bb8e90e9d2b6666f0e
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-08 16:42:41 +0545

    day17 packages

---
+++ day17_restaurant/src/lib.rs
@@ -0,0 +1,20 @@
+mod front_of_house;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn eat_at_restaurant(){
+    front_of_house::hosting::add_to_waitlist();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
