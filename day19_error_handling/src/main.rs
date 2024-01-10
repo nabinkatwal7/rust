b361867cda4dffe65e5205d3bf78080ef67e56e5
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-10 10:42:16 +0545

    day19 error handling

--- day19_error_handling/src/main.rs
+++ day19_error_handling/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
 fn main() {
-    println!("Hello, world!");
+    // panic!("Error occurred, Quit!")
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = greeting_file_result.unwrap_or_else(|error| match error.kind() {
+        ErrorKind::NotFound => match File::create("hello.txt") {
+            Ok(fc) => fc,
+            Err(e) => panic!("Problem creating the file: {:?}", e),
+        },
+        other_error => {
+            panic!("Problem opening the file: {:?}", other_error)
+        }
+    });
+
 }
